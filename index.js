0b247d137a00b36a4bdd468d5cc1fe00fce5388c
Connor Sheets <connorsheets32@gmail.com>
2022-12-29 15:12:06 -0600

    commit tree 3c92cae1e95a0d0d4588034dd7e574627bd38293

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.body.removeChild(main);
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "Connor is the champion"
